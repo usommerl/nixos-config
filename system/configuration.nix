5405be6a59ddd9c1e54a52f5b0c93c15598eb11a
Uwe Sommerlatt <uwe.sommerlatt@gmail.com>
2023-11-07 19:14:59 +0100

    Reorganise configuration.nix, add automatic gc and nix store optimisation

--- system/configuration.nix
+++ system/configuration.nix
@@ -2,19 +2,20 @@
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
       ./hardware-configuration.nix
     ];
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix = {
+    settings.experimental-features = [ "nix-command" "flakes" ];
+    settings.auto-optimise-store = true;
 
-  environment.systemPackages = with pkgs; [
-    neovim
-    git
-    curl
-  ];
-
-  environment.variables.EDITOR = "nvim";
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delte-older-than 2w";
+    };
+  };
 
   boot = {
     consoleLogLevel = 0;
@@ -23,67 +24,56 @@
     kernelParams = [ "quiet" "splash" "rd.systemd.show_status=false" "rd.udev.log_level=3" "udev.log_priority=3" "boot.shell_on_fail" ];
 
     loader = {
-      timeout = 0;
       systemd-boot.enable = true;
+      systemd-boot.configurationLimit = 15;
       efi.canTouchEfiVariables = true;
+      timeout = 0;
     };
   };
 
-  networking.hostName = "ares";
-  networking.networkmanager.enable = true;
+  environment.variables.EDITOR = "nvim";
+  environment.systemPackages = with pkgs; [
+    neovim
+    git
+    curl
+  ];
 
-  # Set your time zone.
   time.timeZone = "Europe/Berlin";
 
-  # Select internationalisation properties.
+
+  networking.hostName = "ares";
+  networking.networkmanager.enable = true;
+  networking.firewall.enable = false;
+
+
   i18n.defaultLocale = "en_US.UTF-8";
   console = {
     font="Lat2-Terminus16";
     useXkbConfig = true;
   };
 
-  # Enable and configure the X11 windowing system.
   services.xserver = {
     enable = true;
     layout = "de";
     xkbVariant = "nodeadkeys";
-   
+    
+    libinput.enable = false;
     displayManager.startx.enable = true;
     windowManager.awesome.enable = true;
   };
 
-  # Enable CUPS to print documents.
   services.printing.enable = true;
-
-  # Enable sound.
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
-
-  # Disable touchpad support (enabled default in most desktopManager).
-  services.xserver.libinput.enable = false;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.uwe = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" "networkmanager" ];
-    packages = with pkgs; [
-    ];
-  };
-
-
-  # Enable the OpenSSH daemon.
   services.openssh.enable = true;
   services.openssh.settings.PermitRootLogin = "no";
 
-  # Disable the firewall
-  networking.firewall.enable = false;
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It's perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.05"; # Did you read the comment?
+  users.users.uwe = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" ];
+  };
+
+  system.stateVersion = "23.05";
 }
 
