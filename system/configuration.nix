31b49a33c49a4931f0f89ac1f1f1048b99e226a5
Uwe Sommerlatt <uwe.sommerlatt@gmail.com>
2023-11-13 20:21:03 +0100

    Use latest kernel

--- system/configuration.nix
+++ system/configuration.nix
@@ -21,6 +21,7 @@
     consoleLogLevel = 0;
     initrd.verbose = false;
     plymouth.enable = true;
+    kernelPackages = pkgs.linuxPackages_latest;
     kernelParams = [ "quiet" "splash" "rd.systemd.show_status=false" "rd.udev.log_level=3" "udev.log_priority=3" "boot.shell_on_fail" ];
 
     loader = {
