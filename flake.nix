ccc41449b68609fe02647ceddf13db711bdbf721
Uwe Sommerlatt <uwe.sommerlatt@gmail.com>
2023-12-16 17:57:02 +0100

    Refactor flake.nix

--- flake.nix
+++ flake.nix
@@ -3,8 +3,12 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     hyprland = {
       url = "github:hyprwm/Hyprland";
       inputs.nixpkgs.follows = "nixpkgs";
