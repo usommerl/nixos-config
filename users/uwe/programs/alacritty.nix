9670476b10a3f6e0f7ea44e6e168500f03040bfe
Uwe Sommerlatt <uwe.sommerlatt@gmail.com>
2023-12-17 15:59:37 +0100

    Increase alacritty scrolling history

--- users/uwe/programs/alacritty.nix
+++ users/uwe/programs/alacritty.nix
@@ -3,6 +3,7 @@
     enable = true;
     settings = {
       font.normal.family = "JetBrainsMono Nerd Font Mono";
+      scrolling.history = 20000;
       window.padding = {
         x = 4;
         y = 4;
