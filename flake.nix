e71f09513004d02d2fdead73138e0c838f2342d2
Uwe Sommerlatt <uwe.sommerlatt@gmail.com>
2023-12-18 22:25:11 +0100

    Set system.nixos.label

--- flake.nix
+++ flake.nix
@@ -22,6 +22,7 @@
     hyprland, ... 
   }: let 
     username = "uwe";
+    inherit (nixpkgs) lib;
   in {
     nixosConfigurations = {
 
@@ -30,6 +31,12 @@
 	specialArgs = { inherit hyprland username; };
 	modules = [
 	  ./hosts/configuration.nix
+	  ({
+            system.nixos.label = 
+            if self.sourceInfo ? lastModifiedDate && self.sourceInfo ? shortRev
+            then "${lib.substring 0 8 self.sourceInfo.lastModifiedDate}.${self.sourceInfo.shortRev}"
+            else lib.warn "Repo is dirty, revision will not be available in system label" "dirty";
+          })
           home-manager.nixosModules.home-manager {
 	    nixpkgs.config.allowUnfree = true;
             home-manager.useGlobalPkgs = true;
