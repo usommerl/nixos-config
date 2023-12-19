378b99ac1b41553a681fb67cf0339e128176b960
Uwe Sommerlatt <uwe.sommerlatt@gmail.com>
2023-12-19 09:17:34 +0100

    Add timezone designator

--- modules/custom-label.nix
+++ modules/custom-label.nix
@@ -5,7 +5,7 @@
   inherit (self) sourceInfo;
 
   formatDate = s : "${substring 0 4 s}-${substring 4 2 s}-${substring 6 2 s}";
-  formatTime = s : "${substring 8 2 s}:${substring 10 2 s}:${substring 12 2 s}";
+  formatTime = s : "${substring 8 2 s}:${substring 10 2 s}:${substring 12 2 s}Z";
   formatTimestamp = s : "${formatDate s}T${formatTime s}";
 
 in {
