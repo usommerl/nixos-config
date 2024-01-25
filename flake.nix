449c2d079c9ef626053bb25afb66fa81fb4174a7
Uwe Sommerlatt <uwe.sommerlatt@gmail.com>
2024-01-25 21:55:50 +0100

    Remove trailing spaces and tabs from flake.nix

--- flake.nix
+++ flake.nix
@@ -15,30 +15,30 @@
     };
   };
 
-  outputs = inputs@{ 
-    nixpkgs, 
-    home-manager, 
-    ... 
-  }: 
-  let 
+  outputs = inputs@{
+    nixpkgs,
+    home-manager,
+    ...
+  }:
+  let
     inherit (nixpkgs.lib) nixosSystem attrsets;
   in {
     nixosConfigurations = {
 
       ares = nixosSystem rec {
-	system = "x86_64-linux";
-	specialArgs = (attrsets.mergeAttrsList [ { mainUser = "uwe"; hostName = "ares"; } inputs ]);
-	modules = [
-	  ./hosts/${specialArgs.hostName}
-          home-manager.nixosModules.home-manager 
-	  {
+  system = "x86_64-linux";
+  specialArgs = (attrsets.mergeAttrsList [ { mainUser = "uwe"; hostName = "ares"; } inputs ]);
+  modules = [
+    ./hosts/${specialArgs.hostName}
+          home-manager.nixosModules.home-manager
+    {
             nixpkgs.config.allowUnfree = true;
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-	    home-manager.extraSpecialArgs = specialArgs;
+      home-manager.extraSpecialArgs = specialArgs;
             home-manager.users.${specialArgs.mainUser} = import ./users/${specialArgs.mainUser};
           }
-	];
+  ];
       };
     };
   };
