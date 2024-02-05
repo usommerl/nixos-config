bca41a425af22079a4c213b1201b2b1cb8d07b93
Uwe Sommerlatt <uwe.sommerlatt@gmail.com>
2024-02-05 23:43:17 +0100

    Change alacritty colorscheme

--- users/uwe/programs/alacritty.nix
+++ users/uwe/programs/alacritty.nix
@@ -1,8 +1,15 @@
-{ mainFontName, ... }:
+{ mainFontName, pluginFromGitHub, ... }:
+let
+  # I need a more recent version that contains alacritty colorschemes in toml format
+  tokyonight = (pluginFromGitHub "610179f7f12db3d08540b6cc61434db2eaecbcff" "main" "folke/tokyonight.nvim");
+in
 {
   programs.alacritty = {
     enable = true;
     settings = {
+      import = [
+        "${tokyonight}/extras/alacritty/tokyonight_moon.toml"
+      ];
       font.normal.family = "${mainFontName}";
       scrolling.history = 20000;
       window.padding = {
