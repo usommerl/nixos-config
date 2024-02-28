ced3794349876da1a5fa90ae01c8880a879010c3
Uwe Sommerlatt <uwe.sommerlatt@gmail.com>
2024-02-28 21:03:01 +0100

    Modify swaylock styling

--- users/uwe/programs/swaylock.nix
+++ users/uwe/programs/swaylock.nix
@@ -1,15 +1,38 @@
 { mainFontName, pkgs, config, ... }:
 let
-  lockCmd = "${pkgs.swaylock}/bin/swaylock --daemonize --config ${config.xdg.configHome}/swaylock/config";
+  swaylockPkg = pkgs.swaylock-effects;
+  lockCmd = "${swaylockPkg}/bin/swaylock --daemonize --config ${config.xdg.configHome}/swaylock/config";
+
+  white = "FFFFFFFF";
+  black = "000000FF";
+  darkGrey = "888888FF";
+  yellow = "FFA000FF";
 in
 {
   programs.swaylock = {
     enable = true;
+    package = swaylockPkg;
     settings = {
-      color = "000000";
-      font = "${mainFontName}";
-      font-size = 24;
+      color = black;
+      ring-color = white;
+      ring-clear-color = white;
+      ring-caps-lock-color = white;
+      ring-ver-color = white;
+      ring-wrong-color = white;
+      inside-clear-color = black;
+      inside-ver-color = darkGrey;
+      inside-wrong-color = yellow;
+      text-caps-lock-color = yellow;
+      text-clear-color = white;
+      text-ver-color = white;
+      bs-hl-color = darkGrey;
+      key-hl-color = yellow;
+      font = mainFontName;
+      font-size = 20;
       indicator-radius = 80;
+      indicator-thickness = 16;
+      ignore-empty-password = true;
+      text-ver = "";
     };
   };
 
