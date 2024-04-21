ba9638f1ec4a147e3364b5ab51f63c0f3fd13ed4
Uwe Sommerlatt <uwe.sommerlatt@gmail.com>
2024-04-21 21:14:05 +0200

    Create a desktop file for Google Chrome

--- users/uwe/programs/google-chrome.nix
+++ users/uwe/programs/google-chrome.nix
@@ -1,7 +1,36 @@
 { pkgs, ... }:
+with pkgs;
 {
-  home.packages = with pkgs; [
+  # xdg-open is looking for a 'google-chrome' command
+  home.packages = [
     (writeShellScriptBin "google-chrome" "exec -a $0 ${google-chrome}/bin/google-chrome-stable $@")
-    (writeShellScriptBin "chrome" "exec -a $0 ${google-chrome}/bin/google-chrome-stable $@")
   ];
+
+  # To start the Google Chrome browser via an application runner
+  xdg.desktopEntries.chrome = {
+    name = "chrome";
+    genericName = "Web Browser";
+    icon = "google-chrome";
+    type = "Application";
+    exec = "${google-chrome}/bin/google-chrome-stable";
+    terminal = false;
+    startupNotify = true;
+    categories = [ "Application" "Network" "WebBrowser" ];
+    mimeType = [
+      "application/pdf"
+      "application/rdf+xml"
+      "application/rss+xml"
+      "application/xhtml+xml"
+      "application/xhtml_xml"
+      "application/xml"
+      "image/gif"
+      "image/jpeg"
+      "image/png"
+      "image/webp"
+      "text/html"
+      "text/xml"
+      "x-scheme-handler/http"
+      "x-scheme-handler/https"
+    ];
+  };
 }
