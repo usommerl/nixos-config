7054c2748515ff3f333aa49e4aba54ec46043b61
Uwe Sommerlatt <uwe.sommerlatt@gmail.com>
2024-04-21 19:04:34 +0200

    Remove let block

--- users/uwe/mounts/harpocrates.nix
+++ users/uwe/mounts/harpocrates.nix
@@ -1,21 +1,18 @@
 { config, ... }:
 let
   user = "uwe";
+  uid = toString config.users.users.${user}.uid;
 in
 {
   # TODO: Credentials file?
   fileSystems."/mnt/${user}/harpocrates" = {
     device = "//harpocrates.tail15a8b.ts.net/homes/${user}";
     fsType = "cifs";
-    options =
-      let
-        uid = toString config.users.users.${user}.uid;
-      in
-      [
-        "noauto,x-systemd.automount"
-        "x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s"
-        "uid=${uid},gid=100,dir_mode=0700,file_mode=0600,nobrl"
-        "credentials=/home/${user}/.private/harpocrates"
-      ];
+    options = [
+      "noauto,x-systemd.automount"
+      "x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s"
+      "uid=${uid},gid=100,dir_mode=0700,file_mode=0600,nobrl"
+      "credentials=/home/${user}/.private/harpocrates"
+    ];
   };
 }
