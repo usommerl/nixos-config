9894cac397bbb8f062ec935e2ba46811819b114f
Uwe Sommerlatt <uwe.sommerlatt@gmail.com>
2024-05-05 01:08:54 +0200

    Move key maps for the Linediff plugin to the nix file of the plugin

--- users/uwe/programs/neovim/plugins/linediff.nix
+++ users/uwe/programs/neovim/plugins/linediff.nix
@@ -1,6 +1,13 @@
 { pluginFromGitHub, ... }:
 {
   programs.neovim.plugins = [
-    (pluginFromGitHub "245d16328c47a132574e0fa4298d24a0f78b20b0" "main" "AndrewRadev/linediff.vim")
+    {
+      plugin = (pluginFromGitHub "245d16328c47a132574e0fa4298d24a0f78b20b0" "main" "AndrewRadev/linediff.vim");
+      type = "lua";
+      config = ''
+        vim.keymap.set('n', '<leader>dl', ':Linediff<cr>', { desc = "Diff line" })
+        vim.keymap.set('v', '<leader>dl', ':Linediff<cr>', { desc = "Diff line" })
+      '';
+    }
   ];
 }
