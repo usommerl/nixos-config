a47b3a9f25d5938cf8a848e8596e8263f3bd6ecb
Uwe Sommerlatt <uwe.sommerlatt@gmail.com>
2024-05-05 02:46:48 +0200

    Refactor configuration for Telescope plugin

--- users/uwe/programs/neovim/plugins/telescope.nix
+++ users/uwe/programs/neovim/plugins/telescope.nix
@@ -17,6 +17,46 @@
         config = ''
           -- Telescope plugin --------------------------------------------------------------
 
+          -- See: https://github.com/nvim-telescope/telescope.nvim/issues/2874#issuecomment-1900967890
+          local my_find_files
+          my_find_files = function(opts, no_ignore)
+            opts = opts or {}
+            no_ignore = vim.F.if_nil(no_ignore, false)
+            opts.attach_mappings = function(_, map)
+              map({ "n", "i" }, "<C-h>", function(prompt_bufnr) -- <C-h> to toggle modes
+                local prompt = require("telescope.actions.state").get_current_line()
+                require("telescope.actions").close(prompt_bufnr)
+                no_ignore = not no_ignore
+                my_find_files({ default_text = prompt }, no_ignore)
+              end)
+              return true
+            end
+
+            if no_ignore then
+              opts.no_ignore = true
+              opts.hidden = true
+              opts.prompt_title = "Find Files <ALL>"
+              require("telescope.builtin").find_files(opts)
+            else
+              opts.prompt_title = "Find Files"
+              require("telescope.builtin").find_files(opts)
+            end
+          end
+
+          vim.keymap.set('n', '<leader>ff', my_find_files, { desc = "Telescope find_files" })
+          vim.keymap.set('n', '<leader>fb', ':Telescope buffers<cr>')
+          vim.keymap.set('n', '<leader>fc', ':Telescope colorscheme<cr>')
+          vim.keymap.set('n', '<leader>fg', ':Telescope live_grep<cr>')
+          vim.keymap.set('n', '<leader>fk', ':Telescope keymaps<cr>')
+          vim.keymap.set('n', '<leader>fm', ':Telescope metals commands<cr>')
+          vim.keymap.set('n', '<leader>fr', ':Telescope frecency<cr>')
+          vim.keymap.set('n', '<leader>fR', ':Telescope registers<cr>')
+          vim.keymap.set('n', '<leader>gl', ':<C-u>Telescope git_commits<cr>')
+          vim.keymap.set('n', '<leader>gL', ':<C-u>Telescope git_bcommits<cr>')
+          vim.keymap.set('n', '<leader>gb', ':<C-u>Telescope git_branches<cr>')
+          vim.keymap.set('n', 'Z', ':Telescope zoxide list<cr>')
+          vim.keymap.set('n', '<leader>u', ':Telescope undo<cr>')
+
           local telescope = require('telescope')
 
           telescope.setup {
@@ -48,47 +88,6 @@
           require('telescope._extensions.zoxide.config').setup({
             list_command = 'zoxide query -l --all'
           })
-
-          -- See: https://github.com/nvim-telescope/telescope.nvim/issues/2874#issuecomment-1900967890
-          local my_find_files
-          my_find_files = function(opts, no_ignore)
-            opts = opts or {}
-            no_ignore = vim.F.if_nil(no_ignore, false)
-            opts.attach_mappings = function(_, map)
-              map({ "n", "i" }, "<C-h>", function(prompt_bufnr) -- <C-h> to toggle modes
-                local prompt = require("telescope.actions.state").get_current_line()
-                require("telescope.actions").close(prompt_bufnr)
-                no_ignore = not no_ignore
-                my_find_files({ default_text = prompt }, no_ignore)
-              end)
-              return true
-            end
-
-            if no_ignore then
-              opts.no_ignore = true
-              opts.hidden = true
-              opts.prompt_title = "Find Files <ALL>"
-              require("telescope.builtin").find_files(opts)
-            else
-              opts.prompt_title = "Find Files"
-              require("telescope.builtin").find_files(opts)
-            end
-          end
-
-          -- Telescope keymaps --------------------------------------------------------------
-          vim.keymap.set('n', '<leader>ff', my_find_files, { desc = "Telescope find_files" })
-          vim.keymap.set('n', '<leader>fb', ':Telescope buffers<cr>')
-          vim.keymap.set('n', '<leader>fc', ':Telescope colorscheme<cr>')
-          vim.keymap.set('n', '<leader>fg', ':Telescope live_grep<cr>')
-          vim.keymap.set('n', '<leader>fk', ':Telescope keymaps<cr>')
-          vim.keymap.set('n', '<leader>fm', ':Telescope metals commands<cr>')
-          vim.keymap.set('n', '<leader>fr', ':Telescope frecency<cr>')
-          vim.keymap.set('n', '<leader>fR', ':Telescope registers<cr>')
-          vim.keymap.set('n', '<leader>gl', ':<C-u>Telescope git_commits<cr>')
-          vim.keymap.set('n', '<leader>gL', ':<C-u>Telescope git_bcommits<cr>')
-          vim.keymap.set('n', '<leader>gb', ':<C-u>Telescope git_branches<cr>')
-          vim.keymap.set('n', 'Z', ':Telescope zoxide list<cr>')
-          vim.keymap.set('n', '<leader>u', ':Telescope undo<cr>')
         '';
       }
     ];
