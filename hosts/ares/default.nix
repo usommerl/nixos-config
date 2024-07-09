df5f5a6bbfb0107613f4706bd72be6a754f16d04
Uwe Sommerlatt <uwe.sommerlatt@gmail.com>
2024-07-09 21:42:25 +0200

    Change plymouth theme

--- hosts/ares/default.nix
+++ hosts/ares/default.nix
@@ -2,6 +2,7 @@
 with lib;
 let
   mainUserMountConfig = ../../users/${mainUser}/mounts;
+  plymouthTheme = "hexagon";
 in
 {
   imports =
@@ -34,9 +35,22 @@ in
   boot = {
     consoleLogLevel = 0;
     initrd.verbose = false;
-    plymouth.enable = true;
     kernelPackages = pkgs.linuxPackages_latest;
     kernelParams = [ "quiet" "splash" "rd.systemd.show_status=false" "rd.udev.log_level=3" "udev.log_priority=3" "boot.shell_on_fail" ];
+
+    # See:  
+    # - https://github.com/NixOS/nixpkgs/pull/215693#issuecomment-1547809466
+    # - https://github.com/NixOS/nixpkgs/blob/master/pkgs/data/themes/adi1090x-plymouth-themes/shas.nix
+    # - https://github.com/adi1090x/plymouth-themes?tab=readme-ov-file
+    plymouth = {
+      enable = true;
+      theme = plymouthTheme;
+      themePackages = [
+        (pkgs.adi1090x-plymouth-themes.override {
+          selected_themes = [plymouthTheme];
+        })
+      ];
+    };
 
     loader = {
       systemd-boot.enable = true;
