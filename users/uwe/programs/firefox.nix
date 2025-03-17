f6036d7c682dcb8b3b1841358a64e9aad96f42e9
Uwe Sommerlatt <uwe.sommerlatt@gmail.com>
2025-03-17 18:01:33 +0100

    Set Firefox as default browser

--- users/uwe/programs/firefox.nix
+++ users/uwe/programs/firefox.nix
@@ -20,4 +20,16 @@
       "x-scheme-handler/https"
     ];
   };
+
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "default-web-browser"       = [ "firefox.desktop" ];
+      "text/html"                 = [ "firefox.desktop" ];
+      "x-scheme-handler/http"     = [ "firefox.desktop" ];
+      "x-scheme-handler/https"    = [ "firefox.desktop" ];
+      "x-scheme-handler/about"    = [ "firefox.desktop" ];
+      "x-scheme-handler/unknown"  = [ "firefox.desktop" ];
+    };
+  };
 }
