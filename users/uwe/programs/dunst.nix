4a57dd98f8288b283479bf1256c07d32c26b130f
Uwe Sommerlatt <uwe.sommerlatt@gmail.com>
2025-04-24 19:01:49 +0200

    style(dunst.nix): update frame color and padding for improved aesthetics

--- users/uwe/programs/dunst.nix
+++ users/uwe/programs/dunst.nix
@@ -9,17 +9,18 @@
         transparency = 10;
         separator_height = 1;
         frame_width = 1;
-        frame_color = "#eceff1";
+        frame_color = "#000000";
         font = "${mainFontName} 11";
         show_indicators = "no";
         text_icon_padding = 15;
-        horizontal_padding = 18;
+        horizontal_padding = 15;
         corner_radius = 5;
+        gap_size = 8;
       };
 
       urgency_normal = {
-        background = "#222436";
-        foreground = "#eceff1";
+        background = "#ffffff";
+        foreground = "#000000";
         timeout = 10;
       };
     };
