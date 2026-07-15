f4dad2c26674327bca7ec95d79fad233db3408c5
Uwe Sommerlatt <uwe.sommerlatt@gmail.com>
2026-07-15 23:24:21 +0200

    feat(hyprland): Disable random text on background

--- users/uwe/programs/hyprland.nix
+++ users/uwe/programs/hyprland.nix
@@ -77,6 +77,7 @@
         misc = {
           force_default_wallpaper = 0,
           disable_hyprland_logo = true,
+          disable_splash_rendering = true,
           background_color = 0x222436,
         },
 
